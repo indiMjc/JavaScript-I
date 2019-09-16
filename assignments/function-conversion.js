ee57a5f3e3c0fdf9126341abddabe6e882fac120
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-09-16 16:17:16 -0400

    Arrow functions complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,33 @@
 // };
 // myFunction();
 
+const myFunction = () => {
+    'Function was invoked!'
+}
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (param) =>
+    param;
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (param1, param2) => 
+    param1 + param2;
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const subtract = (param1, param2) => 
+    param1 - param2;
 
 
 // Stretch
